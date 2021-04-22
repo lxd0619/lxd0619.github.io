c556fa5570a6f743be4a57db8b64d0470b3fd879
lixudong <lixudong@fulan.com.cn>
2021-04-22 18:05:45 +0800

    。

--- themes/hexo-theme-next/scripts/events/index.js
+++ themes/hexo-theme-next/scripts/events/index.js
@@ -1,28 +1,27 @@
 /* global hexo */
 
-'use strict';
+"use strict";
 
-hexo.extend.filter.register('before_generate', () => {
-  // Merge config
-  require('./lib/config')(hexo);
-  // Set vendors
-  require('./lib/vendors')(hexo);
-  // Add filter type `theme_inject`
-  require('./lib/injects')(hexo);
-  // Highlight
-  require('./lib/highlight')(hexo);
-}, 0);
+hexo.extend.filter.register(
+  "before_generate",
+  () => {
+    // Merge config
+    require("./lib/config")(hexo);
+    // Set vendors
+    require("./lib/vendors")(hexo);
+    // Add filter type `theme_inject`
+    require("./lib/injects")(hexo);
+    // Highlight
+    require("./lib/highlight")(hexo);
+  },
+  0
+);
 
-hexo.on('ready', () => {
-  const { version } = require('../../package.json');
-  hexo.log.info(`==================================
-  ███╗   ██╗███████╗██╗  ██╗████████╗
-  ████╗  ██║██╔════╝╚██╗██╔╝╚══██╔══╝
-  ██╔██╗ ██║█████╗   ╚███╔╝    ██║
-  ██║╚██╗██║██╔══╝   ██╔██╗    ██║
-  ██║ ╚████║███████╗██╔╝ ██╗   ██║
-  ╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝   ╚═╝
+hexo.on("ready", () => {
+  const { version } = require("../../package.json");
+  hexo.log.info(`
 ========================================
+Compiled successfully！
 NexT version ${version}
 Documentation: https://theme-next.js.org
 ========================================`);
